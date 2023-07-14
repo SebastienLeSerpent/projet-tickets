e3f61ae6ebf65afbe772e7439760f1ae6a26cdd6
Sébastien <75444196+SebastienLeSerpent@users.noreply.github.com>
2023-07-14 11:35:52 +0200

    Create logout.php

---
+++ logout.php
@@ -0,0 +1,14 @@
+<?php
+	// Début de la session
+	session_start();
+
+	// Suppression des données de la session
+	session_unset();
+
+	// Destruction de la session
+	session_destroy();
+
+	// Redirection vers la page de connexion
+	header("Location: page_login.html");
+	exit();
+?>
